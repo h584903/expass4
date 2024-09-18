990a08107917c9ad7910e1eeda1a1d046ee3dc95
installer <installer@gmail.com>
2024-09-18 11:56:41 +0200

    test

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A Nix-flake for fmc";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      javaVersion = 22; # Change this value to update the whole stack
+
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ maven gradle openjdk22 nodejs ];
+          shellHook = ''
+              export JAVA_HOME=${pkgs.openjdk22}
+              PATH="${pkgs.jdk21}/bin:$PATH"
+          '';
+      };
+    };
+}
